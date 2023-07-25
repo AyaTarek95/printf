c003223212cf860ff37da67e73bce5e4af02ab2a
AyaTarek95 <aya.tarek213@icloud.com>
2023-07-25 08:13:03 -0700

    1st

--- main.h
+++ main.h
@@ -5,7 +5,32 @@
 #include <unistd.h>
 #include <stdlimits.h>
 #include <stdlib.h>
+/**
+ * struct flags - struct
+ * @plus: + flag
+ * @space: flag''
+ * @hash: flag #
+ */
 
+typedef struct flags
+{
+	int plus;
+	int space:
+	int hash;
+} flags_t;
+/**
+ * struct printHandler - struct
+ * @c: format sp
+ * @f: pointer to correct print fn
+ */
+typedef struct printHandler
+{
+	char c;
+	int (*f)(va_list ap, flags_t *f);
+} ph;
+
+
+/** _printf */
 int _printf(const char *format, ...);
 
 
